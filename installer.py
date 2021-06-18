209b46e7c34392b5b081511be907249f87e79027
santhanu.csb1923 <santhanu.csb1923@saintgits.org>
2021-06-18 22:40:29 +0530

    Added configuration file and partition file\nAdded documentaion\nFinished the installer

--- installer.py
+++ installer.py
@@ -1,144 +1,34 @@
-import argparse
-import json
-import time
+import functions, sys
+from threading import Thread
+from multiprocessing import Process
 
+"""
+    Calls the functions to install Arch Linux.
+"""
 
-start = time.time()
-#Parse the CLI Aguments 
-def argument_parsing():
-    yes = False
-    parser = argparse.ArgumentParser()
-    #parser.add_argument("mode", help="Select the mode of install.")
-    parser.add_argument('df', help="Sepicify the file contaning dependency information for installation.")
-    parser.add_argument("pf", help="Specify the file that contains partitioning information.")
-    parser.add_argument("pacf", help="Specify the file contaning the packages to download.")
-    parser.add_argument('-y', '--yes', help="Give yes to all Y to all confirmations.", 
-    action="store_true")
-    args = parser.parse_args()
-    
-    if args.yes:
-        yes = args.yes
-    
-    return (args.df, args.pf, args.pacf, yes)
+# Parse the arguments given to the program
+PART_FILE, CONF_FILE, DEFAULT = functions.argument_parsing()
 
+# Settings the files and configurations That is needed for the installation.
+functions.intialize_install(PART_FILE, CONF_FILE, DEFAULT)
 
-#Installing dependencies... This will only work if the internet connection is fine.
-def install_deps(deps_file):
-    from subprocess import Popen, PIPE
+# Create thread to check the internet connection and a process to install the arch system.
+t1 = Thread(target=functions.ping, daemon=True)
+p1 = Process(target=functions.install, daemon=True)
 
-    with open(deps_file) as f:
-        data = f.read()
-        deps = json.loads(data)
+# Thread to print the messages that the threads and the process wants to output.
+# A Queue from the multiprocessing module is used to pass the information. This queue is both thread
+# and process safe.
+t2 = Thread(target=functions.dequeue, args=(t1, p1),daemon=True)
 
-    #Getting the deps dictionary from the dictionary got from file.
-    deps = deps['deps']
+# Starting the process and threads
+t1.start()
+p1.start()
+t2.start()
 
-    print('Installing Dependencies...')
+# Wating for the threads to finish
+t1.join()
+p1.join()
+t2.join()
 
-    proc = Popen(f'pacman -Syy', shell=True, stdout=PIPE, stderr=PIPE)
-    proc.communicate()
-
-    #Refresh package Database
-    if proc.returncode != 0:
-        return [1, '[ERR]Unable to refresh package database.']
-
-    #Install with packman
-    for pac_dep in deps['pacman']:
-        proc = Popen(f'pacman -S --noconfirm {pac_dep}', shell=True, stdout=PIPE, stderr=PIPE)
-        proc.communicate()[1]
-
-        if proc.returncode != 0:
-            return [2, f'[ERR]Unable to Download {pac_dep}. Download Manually and Continue.']
-
-    #Install with pip
-    for pip_dep in deps['pip']:
-        proc = Popen(f'pip install {pip_dep}', shell=True, stdout=PIPE, stdin=PIPE)
-        proc.communicate()[1]
-
-        if proc.returncode != 0:
-            return [3, f'[ERR]Unable to Install {pip_dep}']
-    
-    return [0, '[OK]Dependencies Installed Succesfully.']
-
-## Parse the arguments given to the program
-DEP_FILE, PART_FILE, CONF_FILE, DEFAULT = argument_parsing()
-
-
-#Installing dependencies
-rc, msg = install_deps(DEP_FILE)
-print(msg)
-if rc != 0:
-    exit(2)
-
-
-import functions, classes
-import sys
-
-
-classes.ArchInstaller.set_default(DEFAULT)
-classes.ArchInstaller.set_files(PART_FILE, CONF_FILE)
-
-# Checking IF the system booted in UEFI mode
-EFI_BOOT = functions.verify_efi_boot()
-if EFI_BOOT != True:
-    print("[WARNING]: Boot Mode is not EFI")
-else:
-    print("[OK]Boot Mode is EFI")
-
-
-
-# Starting to Install Arch Linux.
-
-## Check Internet Connection.
-# Returns a list with status and msg.
-status, msg = classes.Ping('archlinux.org').ping()
-print(msg)
-if status != True:
-    sys.exit(1)
-
-
-# Update system clock
-status, msg = functions.update_sys_clock()
-print(msg)
-if status != 0:
-    exit(3)
-
-
-#Create Partition
-part_stat = classes.PartitionMaker().partition()
-if part_stat == -1:
-    exit(-1)
-elif part_stat != 0:
-    exit(4 + part_stat)
-
-
-#Calling Reflector
-#rcode, msg = classes.Command.set_mirrorlist()
-#print(msg)
-#if rcode != 0:
-#    exit(11)
-
-classes.Command.pacstrap()
-print('[OK]Pacstrap Done')
-
-
-rcode = classes.Command.genfstab(uuid=True)
-if rcode != 0:
-    print('[ERR]Unable to genereate fs tab.')
-    exit(12)
-
-print('[OK]Fstab Generated')
-
-rcode = classes.Command.arch_config()
-print(rcode)
-
-
-rcode = classes.Command.pacman()
-print(rcode)
-
-rcode = classes.BootLoader.inst_grub()
-print(rcode)
-
-end = time.time()
-
-print('Time Taken: ', end-start)
+print('Installation Completed Successfully. Please Reboot :)')
